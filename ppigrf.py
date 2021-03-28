859bab5e6d660f8a4633d5688fd8de81c67b0f88
Karl Laundal <karl.laundal@ift.uib.no>
2021-03-28 23:13:19 +0200

    I had been working on the wrong file. Fixed it now

--- ppigrf.py
+++ ppigrf.py
@@ -1,34 +1,74 @@
-""" Script to calcualte IGRF 
+""" 
+MIT License
+
+Copyright (c) 2021 Karl M. Laundal
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
 
 Pure Python implementation of IGRF. 
-
-The code should work with any main field model specified by coefficient in a shc file,
-assuming linear interpolation between model times. 
-
 
 
 Example usage:
 --------------
-from igrf import igrf_gc, igrf
+import numpy as np
+from datetime import datetime
+import ppigrf
 
-Br, Btheta, Bphi = igrf_gc(r, theta, phi, dates) # input in geocentric
-Be, Bn, Bu       = igrf(lon, lat, h, dates) # input in geodetic coordinates
+# GEODETIC
+lon = 5.32415  # degrees east
+lat = 60.39299 # degrees north
+h   = 0        # kilometers above sea level
+date = datetime(2021, 3, 28)
+Be, Bn, Bu = ppigrf.igrf(lon, lat, h, date) # returns east, north, up
+
+# GEOCENTRIC
+r     = 6500 # kilometers from center of Earht
+theta = 30   # colatitude in degrees
+phi   = 4    # degrees east (same as lon)
+Br, Btheta, Bphi = ppigrf.igrf_gc(r, theta, phi, date) # returns radial, south, east
+
+# GRID
+lon = np.array([20, 120, 220])
+lat = np.array([[60, 60, 60], [-60, -60, -60]])
+h   = 0
+dates = [datetime(y, 1, 1) for y in np.arange(1960, 2021, 20)]
+Be, Bn, Bu = ppigrf.igrf(lon, lat, h, dates)
 
 
 
-
-Helper functions are defined first, and then the main IGRF function
+This script contains all code needed to calculate IGRF model values.
+Here is a list of functions: 
 
 Helper functions
 ----------------
-get_legendre         - Calculate Legendre functions with a recursive algorithm found in 
-                       "Spacecraft Attitude Determination and Control" by James Richard Wertz 
-read_shc             - Read spherical harmonic coefficient file (shc format) and return gauss
-                       coefficients as pandas DataFrame 
-yearfrac_to_datetime - Convert fraction of year to datetime
 is_leapyear          - Check if year is leapyear
+yearfrac_to_datetime - Convert fraction of year to datetime
+get_legendre         - Calculate Legendre functions
+read_shc             - Read spherical harmonic coefficient file
 geod2geoc            - Convert from geodetic to geocentric coordinates
 geoc2geod            - Convert from geocentric to geodetic coordinates
+
+Main functions
+--------------
+igrf_gc              - Calculate IGRF model values (geocentric input/output)
+igrf                 - Calculate IGRF model values (geodetic input/output)
 
 """
 
@@ -41,7 +81,7 @@ basepath = os.path.dirname(__file__)
 shc_fn = basepath + '/IGRF13.shc' # Default shc file
 
 # Geomagnetic reference radius:
-REFRE = 6371.2 
+REFRE = 6371.2 # km
 
 # World Geodetic System 84 parameters:
 WGS84_e2 = 0.00669437999014
@@ -50,11 +90,8 @@ WGS84_a  = 6378.137 # km
 
 
 def is_leapyear(year):
-    """ Return True if leapyear else False
-    
-        handles arrays (preserves shape).
+    """ Check for leapyear (handles arrays and preserves shape)
 
-        KML 2016-04-20
     """
 
     # if array:
@@ -81,52 +118,57 @@ def is_leapyear(year):
         return False
 
 
-def yearfrac_to_datetime(number):
-    """ convert fraction of year to datetime 
+def yearfrac_to_datetime(fracyear):
+    """ 
+    Convert fraction of year to datetime 
 
-        handles arrays
+    Parameters
+    ----------
+    fracyear : iterable
+        Date(s) in decimal year. E.g., 2021-03-28 is 2021.2377
+        Must be an array, list or similar.
 
-
-        example:
-
-        >>> dates.yearfrac_to_datetime(np.array([2000.213]))
-        array([datetime.datetime(2000, 3, 18, 22, 59, 31, 200000)], dtype=object)
+    Returns
+    -------
+    datetimes : array
+        Array of datetimes
     """
-    
-    year = np.uint16(number) # truncate number to get year
+
+    year = np.uint16(fracyear) # truncate fracyear to get year
     # use pandas TimedeltaIndex to represent time since beginning of year: 
-    delta_year = pd.TimedeltaIndex((number - year)*(365 + is_leapyear(year)), unit = 'D')
+    delta_year = pd.TimedeltaIndex((fracyear - year)*(365 + is_leapyear(year)), unit = 'D')
     # and DatetimeIndex to represent beginning of years:
     start_year = pd.DatetimeIndex(list(map(str, year)))
  
     # adding them produces the datetime:
     return (start_year + delta_year).to_pydatetime()
 
-def get_legendre(nmax, mmax, theta, schmidtnormalize = True, negative_m = False, minlat = 0, keys = None):
-    """ calculate associated Legendre functions 
 
-        nmax             -- maximum total wavenumber
-        mmax             -- maximum zonal wavenumber
-        theta            -- colatitude in degrees, with N terms
-        schmidtnormalize -- True if Schmidth seminormalization is wanted, False otherwise
-        negative_m       -- True if you want the functions for negative m (complex expansion)
-        keys             -- list of (n, m) tuples to return an array instead of a dict
+def get_legendre(theta, keys):
+    """ 
+    Calculate Schmidt semi-normalized associated Legendre functions
 
-        returns:
-          P, dP -- dicts of legendre functions, and derivatives, with wavenumber tuple as keys
-        
-        or, if keys != None:
-          PdP   -- array of size N, 2*M, where M is the number of terms. The first half of the 
-                   columns are P, and the second half are dP
+    Calculations based on recursive algorithm found in "Spacecraft Attitude Determination and Control" by James Richard Wertz
+    
+    Parameters
+    ----------
+    theta : array
+        Array of colatitudes in degrees
+    keys: iterable
+        list of spherical harmnoic degree and order, tuple (n, m) for each 
+        term in the expansion
 
-
-        algorithm from "Spacecraft Attitude Determination and Control" by James Richard Wertz
-        
-        could be unstable for large nmax...
-
-        KML 2016-04-22
-
+    Returns
+    -------
+    P : array 
+        Array of Legendre functions, with shape (theta.size, len(keys)). 
+    dP : array
+        Array of dP/dtheta, with shape (theta.size, len(keys))
     """
+
+    # get maximum N and maximum M:
+    n, m = np.array([k for k in keys]).T
+    nmax, mmax = np.max(n), np.max(m)
 
     theta = theta.flatten()[:, np.newaxis]
 
@@ -135,9 +177,9 @@ def get_legendre(nmax, mmax, theta, schmidtnormalize = True, negative_m = False,
     sinth = np.sin(d2r*theta)
     costh = np.cos(d2r*theta)
 
-    if schmidtnormalize:
-        S = {}
-        S[0, 0] = 1.
+    # Initialize Schmidt normalization
+    S = {}
+    S[0, 0] = 1.
 
     # initialize the functions:
     for n in range(nmax +1):
@@ -146,7 +188,6 @@ def get_legendre(nmax, mmax, theta, schmidtnormalize = True, negative_m = False,
             dP[n, m] = np.zeros_like(theta, dtype = np.float64)
 
     P[0, 0] = np.ones_like(theta, dtype = np.float64)
-    P[0, 0][np.abs(90 - theta) < minlat] = 0
     for n in range(1, nmax +1):
         for m in range(0, min([n + 1, mmax + 1])):
             # do the legendre polynomials and derivatives
@@ -165,51 +206,49 @@ def get_legendre(nmax, mmax, theta, schmidtnormalize = True, negative_m = False,
                     P[n, m]  = costh * P[n -1, m] - Knm*P[n - 2, m]
                     dP[n, m] = costh * dP[n - 1, m] - sinth * P[n - 1, m] - Knm * dP[n - 2, m]
 
-            if schmidtnormalize:
-                # compute Schmidt normalization
-                if m == 0:
-                    S[n, 0] = S[n - 1, 0] * (2.*n - 1)/n
-                else:
-                    S[n, m] = S[n, m - 1] * np.sqrt((n - m + 1)*(int(m == 1) + 1.)/(n + m))
+            # compute Schmidt normalization
+            if m == 0:
+                S[n, 0] = S[n - 1, 0] * (2.*n - 1)/n
+            else:
+                S[n, m] = S[n, m - 1] * np.sqrt((n - m + 1)*(int(m == 1) + 1.)/(n + m))
 
 
-    if schmidtnormalize:
-        # now apply Schmidt normalization
-        for n in range(1, nmax + 1):
-            for m in range(0, min([n + 1, mmax + 1])):
-                P[n, m]  *= S[n, m]
-                dP[n, m] *= S[n, m]
+    # now apply Schmidt normalization
+    for n in range(1, nmax + 1):
+        for m in range(0, min([n + 1, mmax + 1])):
+            P[n, m]  *= S[n, m]
+            dP[n, m] *= S[n, m]
 
-    if negative_m:
-        for n  in range(1, nmax + 1):
-            for m in range(0, min([n + 1, mmax + 1])):
-                P[n, -m]  = -1.**(-m) * factorial(n-m)/factorial(n+m) *  P[n, m]
-                dP[n, -m] = -1.**(-m) * factorial(n-m)/factorial(n+m) * dP[n, m]
+    Pmat  = np.hstack(tuple(P[key] for key in keys))
+    dPmat = np.hstack(tuple(dP[key] for key in keys)) 
+    return Pmat, dPmat    
 
 
-    if keys is None:
-        return P, dP
-    else:
-        Pmat  = np.hstack(tuple(P[key] for key in keys))
-        dPmat = np.hstack(tuple(dP[key] for key in keys)) 
-    
-        return np.hstack((Pmat, dPmat))
+def read_shc(filename):
+    """ 
+    Read .shc (spherical harmonic coefficient) file
 
+    The function produces data frames that have time as index and 
+    spherical harmonic degree and order as columns. In the case of IGRF,
+    the times will correspond to the different models 5 years apart
 
+    Parameters
+    ----------
+    filename : string
+        filename of .shc file
 
-def read_shc(filename, return_all = False):
-    """ read shc (spherical harmonic coefficient) file
+    Returns
+    -------
+    g : DataFrame
+        pandas DataFrame of gauss coefficients for cos terms.
+    h : DataFrame
+        pandas DataFrame of gauss coefficients for sin terms.
 
-        The function returns two pandas DataFrames, g, and h, containing the gauss coefficients.
-        The column names are (n, m)
-        The index is time (python datetimes).
-
-        SECULAR VARIATION
-
-        if return_all is True, the following will also be returned:
-            N_MIN, N_MAX, NTIMES, SP_ORDER, N_STEPS
-
-        The main point of using this format is that it will be easier to interpolate to other times.
+    Note
+    ----
+    This code does have any special treatment of "secular variation" coefficients. Instead, the SV coefficients of IGRF should be used 
+    to make gauss coefficients. This must be done prior to this code 
+    (when making the .shc file).
     """
 
     header = 2
@@ -233,12 +272,12 @@ def read_shc(filename, return_all = False):
             key = tuple(map(int, line.split()[:2]))
             coeffdict[key] = np.array(list(map(float, line.split()[2:])))
 
-    g = {key:coeffdict[key] for key in coeffdict.keys() if key[1] >= 0}               # g coefficients
-    h = {(key[0], -key[1]):coeffdict[key] for key in coeffdict.keys() if key[1] < 0 } # h coefficients
+    g = {key:coeffdict[key] for key in coeffdict.keys() if key[1] >= 0}
+    h = {(key[0], -key[1]):coeffdict[key] for key in coeffdict.keys() if key[1] < 0 }
     for key in [k for k in g.keys() if k[1] == 0]: # add zero coefficients for m = 0 in h dictionary
         h[key] = 0
 
-    # g and h should now have the same number of keys, so this check should never fail:
+    # this must be true:
     assert len(g.keys()) == len(h.keys())
 
     gdf = pd.DataFrame(g, index = times)
@@ -253,22 +292,35 @@ def read_shc(filename, return_all = False):
 
 def geod2geoc(gdlat, height, Bn, Bu):
     """
-    theta, r, B_th, B_r = geod2lat(gdlat, height, X, Z)
+    Convert from geocentric to geodetic coordinates
 
-       INPUTS:    
-       gdlat is geodetic latitude (not colat)
-       height is geodetic height (km)
-       Bn is northward vector component in geodetic coordinates 
-       Bu is upward vector component in geodetic coordinates
+    Based on Matlab code by Nils Olsen, DTU
 
-       OUTPUTS:
-       theta is geocentric colatitude (degrees)
-       r is geocentric radius (km)
-       B_th is geocentric southward component (theta direction)
-       B_r is geocentric radial component
+    Example:
+    --------
+    theta, r, B_th, B_r = geod2lat(gdlat, height, Bn, Bu)
 
+    Parameters
+    ----------
+    gdlat : array
+        Geodetic latitude [degrees]
+    h : array
+        Height above ellipsoid [km]
+    Bn : array
+        Vector in northward direction, relative to ellipsoid
+    Bu : array
+        Vector in upward direction, relative to ellipsoid
 
-    after Matlab code by Nils Olsen, DTU
+    Returns
+    -------
+    theta : array
+        Colatitudes [degrees]
+    r : array
+        Radius [km]
+    B_th : array
+        Vector component in theta direction
+    B_r : array
+        Vector component in radial direction
     """
 
     a = WGS84_a
@@ -295,22 +347,35 @@ def geod2geoc(gdlat, height, Bn, Bu):
  
 def geoc2geod(theta, r, B_th, B_r):
     """
+    Convert from geodetic to geocentric coordinates
+
+    Based on Matlab code by Nils Olsen, DTU
+
+    Example:
+    --------
     gdlat, height, Bn, Bu = geod2lat(theta, r, B_th, B_r)
 
-       INPUTS:    
-       theta is geocentric colatitude (degrees)
-       r is geocentric radius (km)
-       B_r is geocentric radial component
-       B_th is geocentric southward component (theta direction)
+    Parameters
+    ----------
+    theta : array
+        Colatitudes [degrees]
+    r : array
+        Radius [km]
+    B_th : array
+        Vector component in theta direction
+    B_r : array
+        Vector component in radial direction
 
-       OUTPUTS:
-       gdlat is geodetic latitude (degrees, not colat)
-       height is geodetic height (km)
-       Bn is northward vector component in geodetic coordinates 
-       Bu is upward vector component in geodetic coordinates
-
-
-    after Matlab code by Nils Olsen, DTU
+    Returns
+    -------
+    gdlat : array
+        Geodetic latitude [degrees]
+    h : array
+        Height above ellipsoid [km]
+    Bn : array
+        Vector in northward direction, relative to ellipsoid
+    Bu : array
+        Vector in upward direction, relative to ellipsoid
     """
     
     a = WGS84_a
@@ -370,21 +435,40 @@ def geoc2geod(theta, r, B_th, B_r):
 
 
 def igrf_gc(r, theta, phi, date, coeff_fn = shc_fn):
-    """ compute magnetic field components at given dates, at given geocentric latitude and longitude:
+    """
+    Calculate IGRF model components
 
-        dates: python datetime(s). Can be an array (shape N, dim <= 1)
-        glat:  geocentric latitude(s). Can be an array (shape M, dim <= 1)
-        glon: geocentric longitude(s). Can be an array (shape M, dim <= 1)
-        r: geocentric radius in km. Can be array (shape M, dim <= 1)
-        coeffs: either tuple containing g and, h, or path to shc-file
-        sp_order: order of the spline interpolation (DOESN'T WORK)
+    Input and output in geocentric coordinates
 
-        r, theta, phi must be broadcastable
-        r must be in km, theta and phi in degrees
+    Broadcasting rules apply for coordinate arrays, and the
+    combined shape will be preserved. The dates are kept out
+    of the broadcasting, so that the output will have shape
+    (N, ...) where N is the number of dates, and ... represents
+    the combined shape of the coordinates. If you pass scalars,
+    the output will be arrays of shape (1, 1)
+    
+    Parameters
+    ----------
+    r : array
+        radius [km] of IGRF calculation
+    theta : array
+        colatitude [deg] of IGRF calculation
+    phi : array
+        longitude [deg], positive east, of IGRF claculation
+    date : date(s)
+        one or more dates to evaluate IGRF coefficients
+    coeff_fn : string, optional
+        filename of .shc file. Default is latest IGRF
 
-        returns: X, Y, Z (north, east, down)
-                 these are arrays with shape (N, M), where N is space and M is time
-
+    Return
+    ------
+    Br : array
+        Magnetic field [nT] in radial direction
+    Btheta : array
+        Magnetic field [nT] in theta direction (south on an
+        Earth-centered sphere with radius r)
+    Bphi : array
+        Magnetic field [nT] in eastward direction
     """
 
     # read coefficient file:
@@ -416,8 +500,7 @@ def igrf_gc(r, theta, phi, date, coeff_fn = shc_fn):
     N, M = np.max(n), np.max(m)
 
     # get the legendre functions
-    PdP = get_legendre(N, M, theta, schmidtnormalize = True, keys = g.keys())
-    P, dP = np.split(PdP, 2, axis = 1)
+    P, dP = get_legendre(theta, g.keys())
 
     # Append coefficients at desired times (skip if index is already in coefficient data frame):
     index = g.index.union(date)
@@ -455,8 +538,42 @@ def igrf_gc(r, theta, phi, date, coeff_fn = shc_fn):
     return Br.reshape(outshape), Btheta.reshape(outshape), Bphi.reshape(outshape)
 
 
-def igrf(lon, lat, h, date):
+def igrf(lon, lat, h, date, coeff_fn = shc_fn):
     """
+    Calculate IGRF model components
+
+    Input and output in geodetic coordinates
+
+    Broadcasting rules apply for coordinate arrays, and the
+    combined shape will be preserved. The dates are kept out
+    of the broadcasting, so that the output will have shape
+    (N, ...) where N is the number of dates, and ... represents
+    the combined shape of the coordinates. If you pass scalars,
+    the output will be arrays of shape (1, 1)
+    
+    Parameters
+    ----------
+    lon : array
+        longitude [deg], postiive east, of IGRF calculation
+    lat : array
+        geodetic latitude [deg] of IGRF calculation
+    h : array
+        height [km] above ellipsoid for IGRF calculation
+    date : date(s)
+        one or more dates to evaluate IGRF coefficients
+    coeff_fn : string, optional
+        filename of .shc file. Default is latest IGRF
+
+    Return
+    ------
+    Be : array
+        Magnetic field [nT] in eastward direction
+    Bn : array
+        Magnetic field [nT] in northward direction, relative to
+        ellipsoid
+    Bu : array
+        Magnetic field [nT] in upward direction, relative to 
+        ellipsoid
     """
 
     # convert input to arrays and cast to same shape:
@@ -470,7 +587,7 @@ def igrf(lon, lat, h, date):
     phi = lon
 
     # calculate geocentric components of IGRF:
-    Br, Btheta, Bphi = igrf_gc(r, theta, phi, date)
+    Br, Btheta, Bphi = igrf_gc(r, theta, phi, date, coeff_fn = coeff_fn)
     Be = Bphi
 
     # convert output to geodetic

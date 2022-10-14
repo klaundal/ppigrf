8685c9befd048b434046cc8c3401f069f1d697d4
Santiago Soler <santiago.r.soler@gmail.com>
2022-10-14 11:06:38 -0700

    Add test against 2020-01-01 that doesn't involve date interpolation

--- test/data/README.md
+++ test/data/README.md
@@ -2,14 +2,16 @@
 
 ## Precomputed IGRF
 
-The `b_e.csv`, `b_n.csv` and the `b_z.csv` files contain precomputed values of
-the IGRF field obtained through the [NCEI Geomagnetic
+Each one of these folders contain a set of `b_e.csv`, `b_n.csv` and `b_z.csv`
+files that host precomputed values of the IGRF field obtained through the [NCEI
+Geomagnetic
 Calculator](https://www.ngdc.noaa.gov/geomag/calculators/magcalc.shtml)
 provided by [NOAA](https://www.ngdc.noaa.gov).
 
 These values were computed in a regular grid with a spacing of 1 degree in both
 longitudinal and latitudinal directions, at a height of 5km above the WGS84
-ellipsoid and dated on October 5, 2022.
+ellipsoid. The date of each IGRF field files is specified in the folder name
+following the `YYYY-MM-DD` format.
 
 The `b_z.csv` contains the **downward** components of the magnetic vector on
 each location.
--- test/test_igrf.py
+++ test/test_igrf.py
@@ -7,6 +7,7 @@ import numpy as np
 import numpy.testing as npt
 import pandas as pd
 from pathlib import Path
+from datetime import datetime
 
 from ppigrf import igrf
 from ppigrf.ppigrf import yearfrac_to_datetime
@@ -16,40 +17,66 @@ TEST_DIR = Path(os.path.dirname(__file__))
 TEST_DATA_DIR = TEST_DIR / "data"
 
 
+def load_precomputed_igrf(date):
+    """
+    Loads the precomputed IGRF files from a given date
+
+    Available dates:
+        * 2020-01-01
+        * 2022-10-05
+
+    Parameters
+    ----------
+    date : :class:`datetime.datetime` object
+        Date of the precomputed IGRF field files that will be loaded.
+
+    Returns
+    -------
+    igrf_precomputed : :class:`pandas.Dataframe`
+        Dataframe containing the precomputed values of the IGRF on the given
+        date.
+    """
+    # Read the csv files
+    date_dir = TEST_DATA_DIR / date.strftime("%Y-%m-%d")
+    first_columns = ["date", "latitude", "longitude", "altitude_km"]
+    components = ("b_e", "b_n", "b_z")
+    dataframes = []
+    for component in components:
+        columns = first_columns + [component, component + "_sv"]
+        fname = date_dir / f"{component}.csv"
+        df = pd.read_csv(fname, skiprows=13, names=columns)
+        dataframes.append(df)
+    # Merge the dataframes
+    igrf_precomputed = pd.merge(dataframes[0], dataframes[1])
+    igrf_precomputed = pd.merge(igrf_precomputed, dataframes[-1])
+    # Convert the data in the dataframe into a datetime object
+    decimal_date = igrf_precomputed.date.values[0]
+    (date,) = yearfrac_to_datetime([decimal_date])
+    igrf_precomputed = igrf_precomputed.assign(date=date)
+    return igrf_precomputed
+
+
 class TestIGRFKnownValues:
     """
     Test the IGRF field against precomputed values
     """
 
-    @pytest.fixture
-    def precomputed_igrf(self):
-        """
-        Load precomputed values of the IGRF field
-        """
-        # Read the csv files
-        first_columns = ["date", "latitude", "longitude", "altitude_km"]
-        components = ("b_e", "b_n", "b_z")
-        dataframes = []
-        for component in components:
-            columns = first_columns + [component, component + "_sv"]
-            fname = TEST_DATA_DIR / f"{component}.csv"
-            df = pd.read_csv(fname, skiprows=13, names=columns)
-            dataframes.append(df)
-        # Merge the dataframes
-        igrf_precomputed = pd.merge(dataframes[0], dataframes[1])
-        igrf_precomputed = pd.merge(igrf_precomputed, dataframes[-1])
-        # Convert the data in the dataframe into a datetime object
-        decimal_date = igrf_precomputed.date.values[0]
-        (date,) = yearfrac_to_datetime([decimal_date])
-        igrf_precomputed = igrf_precomputed.assign(date=date)
-        return igrf_precomputed
-
-    def test_igrf(self, precomputed_igrf):
+    @pytest.mark.parametrize(
+        "date",
+        [datetime(2020, 1, 1), datetime(2022, 10, 5)],
+        ids=["2020-01-01", "2022-10-05"],
+    )
+    def test_igrf(self, date):
         """
         Test IGRF against the precomputed values
+
+        The test on 2020-01-01 doesn't involve any interpolation on the
+        dates.
         """
-        # Get the date
-        date = precomputed_igrf.date.values[0]
+        # Get precomputed IGRF field
+        precomputed_igrf = load_precomputed_igrf(date)
+        # Overwrite the date with the one in the data file
+        # date = precomputed_igrf.date.values[0]
         # Compute igrf using ppigrf
         b_e, b_n, b_u = igrf(
             precomputed_igrf.longitude,
